383e7681c6c90c034ba4a88d5056e42f9c590362
Kevin Bartolome <liezelbart142029@gmail.com>
2026-07-24 19:57:35 +0800

    added routes for admin side for both room-types and room

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\RoomController;
+use App\Http\Controllers\Admin\RoomTypeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::resource('room-types', RoomTypeController::class);
+    Route::resource('rooms', RoomController::class);
 });
 
 require __DIR__.'/auth.php';
