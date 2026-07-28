d44cb279383c9c79db919e7b1afe49696110fad6
Kevin Bartolome <liezelbart142029@gmail.com>
2026-07-28 22:25:15 +0800

    new tenant model with its attrivutes an function

---
+++ app/Models/Tenant.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tenant extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'phone',
+        'address',
+        'birthdate',
+        'gender',
+        'emergency_name',
+        'emergency_phone',
+        'emergency_relationship',
+        'id_type',
+        'id_number',
+        'move_in_date',
+        'status',
+        'notes',
+    ];
+
+    protected $casts = [
+        'birthdate' => 'date',
+        'move_in_date' => 'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getFullNameAttribute(): string
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
+}
