d5170710589e7ce53d91b560ad7d67b33622cf79
Kevin Bartolome <liezelbart142029@gmail.com>
2026-07-28 22:22:01 +0800

    added the tenant model

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,4 +28,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function tenant()
+    {
+        return $this->hasOne(Tenant::class);
+    }
 }
