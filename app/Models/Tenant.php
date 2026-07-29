c4778a22849866bd05f1be0f1e87e00612908808
Kevin Bartolome <liezelbart142029@gmail.com>
2026-07-29 21:50:21 +0800

    added function for leasecontracts and activeleaser

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -38,4 +38,14 @@ class Tenant extends Model
     {
         return "{$this->first_name} {$this->last_name}";
     }
+
+    public function leaseContracts()
+    {
+        return $this->hasMany(LeaseContract::class);
+    }
+
+    public function activeLease()
+    {
+        return $this->hasOne(LeaseContract::class)->where('status', 'active');
+    } 
 }
