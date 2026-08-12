46280703c4e41fb4cf14b514878b8f5616ea8186
Vinco2025 <liezelbart142029@gmail.com>
2026-08-12 11:30:39 +0800

    rename the route for tenant dashboard

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -32,7 +32,7 @@ class AuthenticatedSessionController extends Controller
             return redirect()->route('admin.dashboard');
         }
 
-        return redirect()->route('tenant.maintenance.index');
+        return redirect()->route('tenant.dashboard');
     }
 
     /**
