1fc6290d8290aeeab6a1acb1a0433de68764c359
Vinco2025 <liezelbart142029@gmail.com>
2026-08-14 21:15:58 +0800

    added the passoword controller to update the existing password, and i forgot to add this before

---
+++ app/Http/Controllers/Tenant/PasswordController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Tenant;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class PasswordController extends Controller
+{
+    public function edit()
+    {
+        return view('tenant.password.edit');
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'current_password' => 'required',
+            'password'         => 'required|min:8|confirmed',
+        ]);
+
+        $user = Auth::user();
+
+        if (!Hash::check($request->current_password, $user->password)) {
+            return back()->withErrors(['current_password' => 'Current password is incorrect.']);
+        }
+
+        $user->update(['password' => Hash::make($request->password)]);
+
+        return redirect()->route('tenant.password.edit')
+            ->with('success', 'Password changed successfully.');
+    }
+}
